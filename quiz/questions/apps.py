ee664051bb1eb8ae4dc9d31a9b574b73a2236d55
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-05 01:26:24 +0300

    Edited apps.py for apps questions and tests

--- quiz/questions/apps.py
+++ quiz/questions/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class QuestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'questions'
-    verbose_name = 'Вопросы и ответы к ним'
