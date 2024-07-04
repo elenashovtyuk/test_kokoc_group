80220786ecd3139476c1901ed52a046d7b6d2f91
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-05 01:27:02 +0300

    Create models Test, Question, Answer

--- quiz/questions/models.py
+++ quiz/questions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from tests.models import Test
 
-# Create your models here.
+
+class Question(models.Model):
+    """Модель вопроса квиза."""
+    text_of_question = models.CharField(max_length=120)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.text_of_question)
+
+    def get_answer(self):
+        return self.answer.all()
+
+
+class Answer(models.Model):
+    """Модель ответа на вопрос."""
+    text_of_answer = models.CharField(max_length=120)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answer'
+    )
+
+    def __str__(self):
+        return f'question: {self.question}, answer: {self.text_of_answer}'
--- quiz/tests/models.py
+++ quiz/tests/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Test(models.Model):
+    """Модель теста, квиза."""
+    name = models.CharField(max_length=120)
+    count_of_questions = models.IntegerField
+
+    def __str__(self):
+        return self.name
