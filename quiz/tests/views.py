8a9a198fa0a234f0c6108d484f05ea509e04e6e2
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-05 11:18:59 +0300

    Add views for tests

--- quiz/tests/views.py
+++ quiz/tests/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Test
 # Create your views here.
+
+
+class TestListView(ListView):
+    model = Test
+    template_name = 'tests/main.html'
+
+
+def test_view(request, pk):
+    test = Test.objects.get(pk=pk)
+    return render(request, 'tests/test.html', {'obj': test})
