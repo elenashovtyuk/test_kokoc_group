69e71595cb402cdc35f8e70fc5c398bf8625b65f
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-06 13:08:26 +0300

    edited views for tests app

--- quiz/tests/views.py
+++ quiz/tests/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+# from django.views.generic import ListView
 from .models import Test
+from questions.models import Question, Answer
+# from django.http import JsonResponse
 # Create your views here.
 
 
@@ -18,10 +20,17 @@ def list_tests(request):
 
 
 def test_view(request, pk):
-    test = get_object_or_404(Test, pk=pk)
+    test = get_object_or_404(Test, id=pk)
     questions = test.questions.all()
+    answers = []
+    for question in questions:
+
+        for answer in question.answers.all():
+            answers.append(answer)
+
     context = {
         'test': test,
-        'questions': questions
+        'questions': questions,
+        'answers': answers
     }
     return render(request, 'tests/test.html', context)
