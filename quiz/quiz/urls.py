33739835421f354bb473b5687891aa3eb53c381a
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-06 22:55:57 +0300

    add paths

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tests.urls'))
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('tests/', include('tests.urls', namespace='tests')),
+    path('', TemplateView.as_view(template_name="home.html"), name="home"),
+
 ]
