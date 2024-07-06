e3b475007466904ff0cdd18d84a5b0267565a512
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2024-07-06 22:56:44 +0300

    delete unused

--- quiz/tests/urls.py
+++ quiz/tests/urls.py
@@ -3,9 +3,10 @@ from .views import (
     list_tests, test_view
 )
 
+app_name = 'tests'
 
 urlpatterns = [
+
     path('', list_tests, name='list-tests'),
     path('<int:pk>/', test_view, name='test-view'),
-    # path('<int:pk>/data/', test_data_view, name='test-data-view')
 ]
